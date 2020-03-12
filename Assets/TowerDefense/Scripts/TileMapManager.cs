9ab0072009194d8697a13b74b73729ceb8ef0ba2
Zbigniew <zbigniewjerzydrozd@gmail.com>
2020-03-12 23:39:09 +0100

    added chunk claiming

--- Assets/TowerDefense/Scripts/TileMapManager.cs
+++ Assets/TowerDefense/Scripts/TileMapManager.cs
@@ -10,18 +10,22 @@ public class TileMapManager : MonoBehaviour
     public GameObject TilemapChunk;
     public GameObject TilemapBlocker;
     public NavMeshSurface surface;
+    private GameObject[,] TileChunksArray;
 
     // Start is called before the first frame update
     void Start() {
-
+        
+        TileChunksArray = new GameObject[TilemapSizeX,TilemapSizeY];
         // Create the game map.
         for (int i = 0; i < TilemapSizeX; i++)
             for (int j = 0; j < TilemapSizeY; j++)
             {
                 GameObject Tile = Instantiate(TilemapChunk, new Vector3((float)i * TilemapChunkSize, 0.1f, (float)j * TilemapChunkSize), Quaternion.identity);
                 Tile.transform.SetParent(transform);
+                TileChunksArray[i,j] = Tile;
+
                 GameObject Block = Instantiate(TilemapBlocker, new Vector3((float)i * TilemapChunkSize, 0.5f, (float)j * TilemapChunkSize), Quaternion.identity);
-                Tile.transform.SetParent(transform);
+                Block.transform.SetParent(Tile.transform);
             }
 
         surface.BuildNavMesh(); // Bake navmesh. Call this everytime there is an update of the map.
