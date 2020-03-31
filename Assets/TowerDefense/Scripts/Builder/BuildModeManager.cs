51089c1b048c747cb0d299eb7eb3aeaad8946dd2
SWi <wieczorek0725@gmail.com>
2020-03-31 19:36:01 +0200

    Revert "Updated BuildModeManager. UI created in script."
    This reverts commit db86774b767c49eb990c0db9cf8dc8d234a9d454.

--- Assets/TowerDefense/Scripts/Builder/BuildModeManager.cs
+++ Assets/TowerDefense/Scripts/Builder/BuildModeManager.cs
@@ -12,63 +12,22 @@ public class BuildModeManager : MonoBehaviour
     [SerializeField]
     private int playerObstacleCost;
     [SerializeField]
-    private Canvas canvas;
-    [SerializeField]
-    private GameEvent BuildingSuccess;
-    [SerializeField]
-    private GameEvent BuildingFailure;
+    private GameObject buildingsPanel;
 
     private ChunkType selectedBuilding;
     private bool buildModeOn;
+    //All three texts are children of buildingsPanel. They are displayed if buildModeOn is set to true. 
     private Text moneyText;
     private Text playerObstacleText;
     private Text turretText;
-    private Text instructionText;
-    private RectTransform canvasRT;
-    private float canvasXSize;
-    private float canvasYSize;
-    private float canvasZPos;
 
     void Start()
     {
         buildModeOn = false;
-        canvasRT = canvas.GetComponent<RectTransform>();
-        canvasXSize = canvasRT.sizeDelta.x / 2.0f;
-        canvasYSize = canvasRT.sizeDelta.y / 2.0f;
-        canvasZPos = canvasRT.localPosition.z;
-        SetTexts();
-    }
-
-    void SetTexts()
-    {
-        moneyText = SetText(-0.7f, -0.85f, "Money", 150, 40);
-        playerObstacleText = SetText(-0.5f, -0.85f, "Press O to choose an Obstacle", 150, 40);
-        turretText = SetText(-0.2f, -0.85f, "Press T to choose a Tower", 150, 40);
-        instructionText = SetText(0.0f, 0.85f, "Building Mode: press Tab", 250, 40);
-        moneyText.text = "Money: $" + money.ToString();
-        UpdateUI();
-    }
-
-
-    /// <summary>
-    /// Creates a new text. x and y determines its position. For x = 0.0, y = 0.0 it would be the center of the canvas,
-    /// for -1.0, -1.0 lower left corner, for 1.0, 1.0 upper right corner.
-    /// </summary>
-    Text SetText(float x, float y, string content, float width, float height)
-    {
-        GameObject newGO = new GameObject(content);
-        newGO.transform.SetParent(canvas.transform);
-        newGO.transform.position = Vector3.zero;
-
-        Text newText = newGO.AddComponent<Text>();
-        newText.text = content;
-        newText.color = Color.black;
-        newText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
-        newText.fontStyle = FontStyle.Bold;
-        newText.fontSize = 15;
-        newText.rectTransform.sizeDelta = new Vector2(width, height);
-        newText.transform.localPosition = new Vector3(canvasXSize * x, canvasYSize * y, canvasZPos);
-        return newText;
+        buildingsPanel.SetActive(buildModeOn);
+        moneyText = buildingsPanel.transform.Find("moneyText").gameObject.GetComponent<Text>();
+        playerObstacleText = buildingsPanel.transform.Find("obstacleText").gameObject.GetComponent<Text>();
+        turretText = buildingsPanel.transform.Find("turretText").gameObject.GetComponent<Text>();
     }
 
     void Update()
@@ -86,29 +45,22 @@ public class BuildModeManager : MonoBehaviour
                 SetTextColours(Color.black, Color.red);
                 
             }
+            moneyText.text = "Money: $" + money.ToString();
 
         }
+
+        // Turning on/off building mode
         if (UpdateModeCond())
         {
             buildModeOn = !buildModeOn;
+            // buildingsPanel should be switched off if player is not in building mode
             selectedBuilding = ChunkType.none;
-
-            UpdateUI();
-
+            buildingsPanel.SetActive(buildModeOn);
             if (buildModeOn)
             {
                 SetTextColours(Color.black, Color.black);
             }
         }
-        
-    }
-
-    //Updates UI according to the state of BuildModeOn
-    void UpdateUI()
-    {
-        playerObstacleText.enabled = BuildModeOn;
-        moneyText.enabled = BuildModeOn;
-        turretText.enabled = BuildModeOn;
     }
 
     //Sets colours of turretText and playerObstacleText
@@ -119,7 +71,7 @@ public class BuildModeManager : MonoBehaviour
     }
 
     // Currently BuildingMode is switched on/off after hitting Tab
-    public bool UpdateModeCond()
+    private bool UpdateModeCond()
     {
         return Input.GetKeyDown(KeyCode.Tab);
     }
@@ -157,22 +109,10 @@ public class BuildModeManager : MonoBehaviour
     public void DecreaseMoney(int decVal)
     {
         money -= decVal;
-        moneyText.text = "Money: $" + money.ToString();
     }
 
     public void AddMoney(int addVal)
     {
         money += addVal;
-        moneyText.text = "Money: $" + money.ToString();
-    }
-
-    public void Success()
-    {
-        BuildingSuccess.Raise();
-    }
-
-    public void Failure()
-    {
-        BuildingFailure.Raise();
     }
 }
--- Assets/TowerDefense/Scripts/Builder/TileBuildHandler.cs
+++ Assets/TowerDefense/Scripts/Builder/TileBuildHandler.cs
@@ -5,25 +5,13 @@ using UnityEngine;
 public class BuildHandler : MonoBehaviour
 {
     private BuildModeManager buildManager;
-    private BoxCollider thisBoxCollider;
     void Start()
     {
         buildManager= GameObject.Find("GameManager").GetComponent<BuildModeManager>();
-        thisBoxCollider = gameObject.AddComponent<BoxCollider>();
-        thisBoxCollider.enabled = false;
-    }
-
-    void Update()
-    {
-        if (buildManager.UpdateModeCond())
-        {
-            thisBoxCollider.enabled = buildManager.BuildModeOn;
-        }
     }
 
     void OnMouseDown()
     {
-
         // Checking if BuildMode is switched on
         if (buildManager.BuildModeOn)
         {
@@ -31,19 +19,11 @@ public class BuildHandler : MonoBehaviour
             System.Tuple<ChunkType, int> selectedBuildingData = buildManager.ActiveBuildingInfo;
             ChunkType building = selectedBuildingData.Item1;
             int money = selectedBuildingData.Item2;
-            Debug.Log("PRESSED ON TILE. SELECTED BUILDING = " + building.ToString());
             if (buildManager.Money >= money)
             {
                 if (gameObject.GetComponent<Chunk>().ChangeType(building))
                 {
                     buildManager.DecreaseMoney(selectedBuildingData.Item2);
-                    buildManager.Success();
-                    Debug.Log("SUCCESS");
-                }
-                else
-                {
-                    buildManager.Failure();
-                    Debug.Log("FAILURE");
                 }
             }
         }
