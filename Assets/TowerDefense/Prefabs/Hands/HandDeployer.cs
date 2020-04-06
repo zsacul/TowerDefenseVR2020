d4682917dfcc43b47bbc5eaa53bd70ede164af23
Zbigniew <zbigniewjerzydrozd@gmail.com>
2020-04-06 21:49:46 +0200

    hand prop deployment server

---
+++ Assets/TowerDefense/Prefabs/Hands/HandDeployer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GOArray
+{
+    public GameObject Prefab;
+    public GameObject Instance;
+    public string description;
+    public bool deployable;
+}
+
+public class HandDeployer : MonoBehaviour
+{
+    [SerializeField]
+    private List<GOArray> PropList;
+    private GameObject CurrentlyDeployed;
+    // Start is called before the first frame update
+    public string HandDeployerName;
+    public int listIterator;
+
+    private void CallKill(GameObject target)
+    {
+        target.GetComponent<PropManager>().Remove();
+    }
+
+    private void CallWakeup(GameObject target)
+    {
+        target.GetComponent<PropManager>().Respawn();
+    }
+
+    private void CallInit(GameObject target)
+    {
+        target.GetComponent<PropManager>().Initialize();
+    }
+
+    public void DeployNth(int Nth)
+    {
+        CallKill(PropList[listIterator].Instance);
+        CallWakeup(PropList[Nth].Instance);
+        listIterator = Nth;
+    }
+
+    public void DeployNext()
+    {
+        CallKill(PropList[listIterator].Instance);
+        listIterator++;
+        if (listIterator == PropList.Count)
+            listIterator = 0;
+        CallWakeup(PropList[listIterator].Instance);
+    }
+
+    void Start()
+    {
+        foreach(GOArray Prop in PropList)
+        {
+            Prop.Instance = Instantiate(Prop.Prefab, transform); // create a new prop.
+            CallInit(Prop.Instance); // initialize the object
+            CallKill(Prop.Instance); // remove the initialized object
+        }
+
+        // finally call Wakeup on the zero'th object
+        CallWakeup(PropList[0].Instance);
+        listIterator = 0;
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
---
+++ Assets/TowerDefense/Prefabs/Hands/PropManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropManager : MonoBehaviour
+{
+    public int colortype;
+    // Start is called before the first frame update
+    public void Remove()
+    {
+        Debug.Log("Remove called");
+        transform.gameObject.SetActive(false);
+
+    }
+    public void Respawn()
+    {
+        Debug.Log("Respawn called");
+        transform.gameObject.SetActive(true);
+    }
+
+    public void Initialize()
+    {
+        Debug.Log("Initialize called");
+        GameObject Cylinder = transform.GetChild(1).gameObject;
+        var HandRenderer = Cylinder.GetComponent<Renderer>();
+        if (colortype == 0)
+            HandRenderer.material.SetColor("_Color", Color.red);
+        if (colortype == 1)
+            HandRenderer.material.SetColor("_Color", Color.green);
+        if (colortype == 2)
+            HandRenderer.material.SetColor("_Color", Color.blue);
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
