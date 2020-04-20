cf0c4736201e2b9a7666f9537cbc21d36e3dd03f
Korudy <konri01@gmail.com>
2020-04-20 15:26:55 +0200

    BaseTower model ElectricTower model Burning BaseTower animation Appearing/disappearing objects animation TreeObstacle model

---
+++ Assets/TowerDefense/Scripts/ElementTowers/LightningVisibility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningVisibility : MonoBehaviour
+{
+    
+    public Vector2 VisibleTimeRange = new Vector2(3000, 5000);
+    public bool ChangeVisibility = true;
+
+    private float changeVisibilityTime = 0;
+    private bool visibility = true;
+    public LineRenderer childLightning;
+    private void Start()
+    {
+        visibility = UnityEngine.Random.Range(0, 2) == 0 ? false : true;
+    }
+    void Update()
+    {
+        if (ChangeVisibility)
+        {
+            if (changeVisibilityTime <= 0)
+            {
+                visibility = !visibility;
+                GetComponent<LineRenderer>().enabled = visibility;
+                if (visibility == false) changeVisibilityTime = UnityEngine.Random.Range(2500, 5000) / 1000f;
+                else changeVisibilityTime = UnityEngine.Random.Range(VisibleTimeRange.x, VisibleTimeRange.y) / 1000f;
+                if (childLightning != null)
+                    childLightning.enabled = visibility;
+            }
+            else
+            {
+                changeVisibilityTime -= Time.deltaTime;
+            }
+        }
+    }
+    
+}
---
+++ Assets/TowerDefense/Scripts/ElementTowers/ObjectVisibility.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectVisibility : MonoBehaviour
+{
+    [SerializeField]
+    Material normalMaterial;
+    [SerializeField]
+    Material visibilityMaterial;
+    [SerializeField]
+    float changingVisibilitySpeed = 0.2f;
+    [SerializeField]
+    float appearingDelay = 0f;
+
+    private bool appear = false;
+    private bool disappear = false;
+    private float invisiblePart = 1;
+    private MeshRenderer r;
+    private SkinnedMeshRenderer sr;
+
+    private void Start()
+    {
+        r = GetComponent<MeshRenderer>();
+        sr = GetComponent<SkinnedMeshRenderer>();
+        if (r == null)
+        {
+            sr.material = visibilityMaterial;
+        }
+        else
+        {
+            r.material = visibilityMaterial;
+        }
+        Invoke("StartAppearing", appearingDelay);
+        visibilityMaterial.SetFloat("Vector1_8D124B45", invisiblePart);
+    }
+
+    void Update()
+    {
+        if(appear)
+        {
+            invisiblePart -= changingVisibilitySpeed * Time.deltaTime;
+            visibilityMaterial.SetFloat("Vector1_8D124B45", invisiblePart);
+            if (invisiblePart <= 0)
+            {
+                if (r == null)
+                {
+                    sr.material = normalMaterial;
+                }
+                else
+                {
+                    r.material = normalMaterial;
+                }
+                appear = false;
+            }
+        }
+        if(disappear)
+        {
+            invisiblePart += changingVisibilitySpeed * Time.deltaTime;
+            visibilityMaterial.SetFloat("Vector1_8D124B45", invisiblePart);
+            if(invisiblePart >= 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void StartDisappearing()
+    {
+        if (!appear)
+        {
+            if (r == null)
+            {
+                sr.material = visibilityMaterial;
+            }
+            else
+            {
+                r.material = visibilityMaterial;
+            }
+            disappear = true;
+        }
+    }
+
+    public void StartAppearing()
+    {
+        appear = true;
+    }
+}
