e0c884e9a974007aaed9dcc77ce68a3b6cc8f510
SWi <wieczorek0725@gmail.com>
2020-04-23 16:06:20 +0200

    Fixed some bugs

---
+++ Assets/TowerDefense/Scripts/Builder/ObstaclePurchase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstaclePurchase : MonoBehaviour {
+    BuildModeManager buildManager;
+    void Start()
+    {
+        buildManager = GameObject.Find("GameManager").GetComponent<BuildModeManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        buildManager.ChooseObstacle();
+    }
+}
---
+++ Assets/TowerDefense/Scripts/Builder/TowerPurchase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerPurchase : MonoBehaviour {
+    BuildModeManager buildManager;
+    void Start()
+    {
+        buildManager = GameObject.Find("GameManager").GetComponent<BuildModeManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //towerPurchaseCanvasCollider.enabled = false;
+        buildManager.ChooseTower();
+    }
+}
