a9942903b6623a6a1af51813db591898853229b6
StDanger <dtraut8@gmail.com>
2020-05-06 15:54:01 +0100

    temp

---
+++ Assets/TowerDefense/Scripts/Lightning/LightningCycle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningCycle : MonoBehaviour
+{
+    [SerializeField]
+    Gradient ambientLightningColors;
+
+    [SerializeField]
+    Gradient dirLightningColors;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
