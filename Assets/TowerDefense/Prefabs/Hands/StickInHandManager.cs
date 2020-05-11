6b1ac6c2bf98c80b07ee3892b387d7ad211b411d
Zbigniew <zbigniewjerzydrozd@gmail.com>
2020-05-11 13:23:15 +0200

    stick grab and stick ungrab

---
+++ Assets/TowerDefense/Prefabs/Hands/StickInHandManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickInHandManager : PropManager
+{
+    public GameObject Debree;
+    private GameObject HandManger;
+    public override void GrabEvent(float input)
+    {
+        //Debug.Log($"overriden gevent {input}");
+        if (input > 0.70f) // The object is grabbed, treat it as usual
+            GizmoAnimation.SetFloat("GripFloat", input);
+        else
+            HandManger.GetComponent<HandDeployer>().DeployNth(0);
+        /* Jeśli puściliśmy obiekt, to tylko powiedzmy o tym naszemu managerowi. On zadba żeby nas wyłączyć i zawołać nasz poweoff */
+    }
+
+    public override void Respawn()
+    {
+        transform.gameObject.SetActive(true); /* ofc chcemy też pokazać swoją rękę */
+        /* wywalić kij który złapaliśmy */
+    }
+
+
+    public override void Remove()
+    {
+        transform.gameObject.SetActive(false);
+        GameObject distpatched = Instantiate(Debree);
+        distpatched.GetComponent<Transform>().position = transform.position;
+        distpatched.GetComponent<Transform>().rotation = transform.rotation;
+        distpatched.SetActive(true);
+    }
+
+    public override void Initialize()
+    {
+        HandManger = transform.parent.gameObject; /* assume that the parent is the hand manager */
+    }
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
