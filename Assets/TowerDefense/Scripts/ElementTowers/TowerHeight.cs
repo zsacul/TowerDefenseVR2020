e61d521d2fe551e95c2644e67828ad012b0cb56f
Korudy <konri01@gmail.com>
2020-05-26 12:44:42 +0200

    prefab set change and random tower height script

---
+++ Assets/TowerDefense/Scripts/ElementTowers/TowerHeight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerHeight : MonoBehaviour
+{
+    public float towerHeight = 1.0f;
+    void Start()
+    {
+        towerHeight = UnityEngine.Random.Range(0.7f, 1.0f);
+        transform.localScale = new Vector3(transform.localScale.x, towerHeight, transform.localScale.z);
+    }
+
+  
+}
