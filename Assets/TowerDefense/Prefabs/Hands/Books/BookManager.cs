b6bd88f47b38b6ba4d883f92076882232fb9e47b
Zbigniew <zbigniewjerzydrozd@gmail.com>
2020-06-04 04:13:11 +0200

    pagegrab, markers, marker deployment

---
+++ Assets/TowerDefense/Prefabs/Hands/Books/BookManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookManager : MonoBehaviour
+{
+    public GameObject Lmarker, Rmarker;
+    public void EnableMarkers()
+    {
+        Lmarker.SetActive(true);
+        Rmarker.SetActive(true);
+    }
+
+    public void DisableMarkers()
+    {
+        Lmarker.SetActive(false);
+        Rmarker.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
