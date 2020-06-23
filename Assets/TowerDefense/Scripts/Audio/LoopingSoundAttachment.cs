30990042a3e6cd2d82dd32e52f14d5acda88f666
DESKTOP-G4BFDQN\StrAnger <dtraut8@gmail.com>
2020-06-23 13:11:07 +0200

    AddedSoundsForSpells

---
+++ Assets/TowerDefense/Scripts/Audio/LoopingSoundAttachment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingSoundAttachment : BaseSoundAttachment
+{
+    [SerializeField]
+    bool playOnAwake = false;
+
+    AudioSource src;
+
+    private void Start()
+    {
+        if(playOnAwake)
+        {
+            Play();
+        }
+    }
+    override public void Play()
+    {
+        AudioManager.Instance.PlayLooped(clipToPlay, this.gameObject, mixer);
+        src = GetComponent<AudioSource>();
+    }
+
+    public void Stop()
+    {
+        if (src != null && src.isPlaying)
+            AudioManager.Instance.FadeOut(this.gameObject, 0.6f);
+    }
+}
---
+++ Assets/TowerDefense/Scripts/Audio/SoundAtLocation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundAtLocation : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> clips;
+
+    [SerializeField]
+    Vector3 locationOverride;
+
+    [SerializeField]
+    protected AudioMixerGroup mixer;
+
+    private void Start()
+    {
+
+    }
+    public void Play(int clipNumber = 0)
+    {
+        if (locationOverride != Vector3.zero)
+        {
+            locationOverride = this.transform.position;
+        }
+        AudioManager.Instance.PlayOnceAtLocation(clips[clipNumber], locationOverride, mixer);
+    }
+
+}
