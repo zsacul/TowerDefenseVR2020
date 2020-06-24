5628b8b8b43ef6151df18ebfed90ab75a16756a4
KrzysiekSlawik <krzysiek.slawik@gmail.com>
2020-06-25 01:08:44 +0200

    new spell system

---
+++ Assets/TowerDefense/Scripts/PositionAsInput/SpellTree/ISpellNode.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISpellNode
+{
+    Dictionary<(int, int), ISpellNode> GetChildren();
+}
---
+++ Assets/TowerDefense/Scripts/PositionAsInput/SpellTree/SpellTree.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpellTree
+{
+    private SpellNode root;
+    private SpellNode step;
+    // Start is called before the first frame update
+    public SpellTree(Spell[] spells)
+    {
+        root = new SpellNode();
+        step = root;
+        foreach(Spell spell in spells)
+        {
+            BuildTree(spell);
+        }
+    }
+    private void BuildTree(Spell spell)
+    {
+        SpellNode current = root;
+        for(int i = 0; i < spell.castSequence.Length; i++)
+        {
+            if(current.children.ContainsKey(spell.castSequence[i]))
+            {
+                current = current.children[spell.castSequence[i]];
+            }
+            else
+            {
+                SpellNode node = new SpellNode();
+                current.children.Add(spell.castSequence[i], node);
+                current = node;
+            }
+        }
+        current.toCast = spell;
+    }
+    public void BeginCast()
+    {
+        step = root;
+    }
+    public (bool,Spell) CastStep(Vector2Int cast)
+    {
+        if(step.children.ContainsKey(cast))
+        {
+            step = step.children[cast];
+            return (true,step.toCast);
+        }
+        else
+        {
+            return (false,null);
+        }
+    }
+    public Vector2Int[] GetNodes()
+    {
+        return step.children.Keys.ToArray();
+    }
+}
