206c028ba8d66ed573d706cba588933194ab64d7
KrzysiekSlawik <krzysiek.slawik@gmail.com>
2020-08-08 15:45:03 +0200

    nodeMenu

---
+++ Assets/TowerDefense/Scripts/UIKrzysiek/NodePos.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class NodePos
+{
+    public GameObject node;
+    public Vector3 relativePosition;
+}
