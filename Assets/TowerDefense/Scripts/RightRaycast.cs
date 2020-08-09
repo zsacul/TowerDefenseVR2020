2b1d10e07334726d8c2e1587ff4bcd6ade218a7f
SWi <wieczorek0725@gmail.com>
2020-08-09 14:28:41 +0200

    RightRaycast singleton

---
+++ Assets/TowerDefense/Scripts/RightRaycast.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RightRaycast : MonoBehaviour
+{
+    private bool teleportState;
+    private bool buildingState;
+
+    public UnityEvent TeleportOn;
+    public UnityEvent TeleportOff;
+    public UnityEvent BuildingOn;
+    public UnityEvent BuildingOff;
+
+    public static RightRaycast Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        teleportState = true;
+        buildingState = false;
+    }
+
+    public void TurnTeleport(bool newState)
+    {
+        Debug.Log("TurnTeleport");
+        if (newState && !teleportState)
+        {
+            Debug.Log("TurnOnTeleport");
+            TeleportOn.Invoke();
+            teleportState = true;
+        }
+        else if (!newState && teleportState)
+        {
+            TeleportOff.Invoke();
+            teleportState = false;
+        }
+    }
+
+    public void TurnBuilding(bool newState)
+    {
+        if (newState && !buildingState)
+        {
+            BuildingOn.Invoke();
+            buildingState = true;
+        }
+        else if (!newState && buildingState)
+        {
+            BuildingOff.Invoke();
+            buildingState = false;
+        }
+    }
+}
