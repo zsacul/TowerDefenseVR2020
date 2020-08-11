0023fa695d14133a4653216d226ea992f41e15dc
KrzysiekSlawik <krzysiek.slawik@gmail.com>
2020-08-11 22:50:39 +0200

    working on magic

---
+++ Assets/TowerDefense/Scripts/PositionAsInput/DamagingRayV2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DamagingRayV2 : MonoBehaviour, IChargable
+{
+    public GameObject projectile;
+    public UnityEvent pulse;
+    public UnityEvent hit;
+    public UnityEvent startPulsing;
+    public UnityEvent endPulsing;
+    public float repeatRate = 0.5f;
+    public float length;
+
+    public void Release()
+    {
+        StartPulsing();
+        Invoke("EndPulsing", length);
+    }
+
+    public void SetCharge(float charge)
+    {
+
+    }
+    void Pulse()
+    {
+        pulse.Invoke();
+        Instantiate(projectile, transform.position, transform.rotation);
+    }
+    void StartPulsing()
+    {
+        InvokeRepeating("Pulse", 0, repeatRate);
+        startPulsing.Invoke();
+    }
+    void EndPulsing()
+    {
+        if (IsInvoking("Pulse"))
+        {
+            CancelInvoke("Pulse");
+            endPulsing.Invoke();
+        }
+        Destroy(this);
+        Destroy(GetComponent<Renderer>());
+        Destroy(gameObject, 2);
+    }
+}
