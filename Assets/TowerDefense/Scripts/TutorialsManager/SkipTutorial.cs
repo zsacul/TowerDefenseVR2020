8fe82f8cd98759fe1aeb7f8799801473f39db3ed
Unknown <uk@uk.com>
2020-08-11 15:08:06 +0200

    tutorial skip button

---
+++ Assets/TowerDefense/Scripts/TutorialsManager/SkipTutorial.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipTutorial : MonoBehaviour
+{
+    private bool pushed;
+
+    private void Start()
+    {
+        pushed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "HandCollider" && !pushed)
+        {
+            pushed = true;
+            GameObject.Find("CrossbowTutorial").GetComponent<CrossbowMainNode>().SetTutorialDone();
+            GameObject.Find("TutorialFirstTeleport").GetComponent<TeleportMainNode>().SetTutorialDone();
+            GameObject.Find("BuildingTutorial").GetComponent<BuildTutorialManager>().EndTutorial();
+            transform.localPosition += new Vector3(0f, -0.1f, 0f);
+        }
+    }
+
+}
