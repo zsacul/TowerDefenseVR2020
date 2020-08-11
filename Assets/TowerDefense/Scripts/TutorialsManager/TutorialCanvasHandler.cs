6805547ac294b9638c16a4152f3e51de67a3a5f3
Unknown <uk@uk.com>
2020-08-11 14:36:39 +0200

    text after tutorial

---
+++ Assets/TowerDefense/Scripts/TutorialsManager/TutorialCanvasHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCanvasHandler : GameEventListener
+{
+    public override void OnEventRaised(Object data)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/TowerDefense/Scripts/TutorialsManager/TutorialText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialText : MonoBehaviour
+{
+    public TextMeshProUGUI textHolder;
+    public string TextAftertutorial;
+
+    public void SetText()
+    {
+        textHolder.text = TextAftertutorial;
+    }
+}
