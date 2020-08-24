0fb65c021aeeb5082230e12bb8a9273cfa9c0075
SWi <wieczorek0725@gmail.com>
2020-08-24 19:45:31 +0200

    endgame text displayed

---
+++ Assets/TowerDefense/Scripts/Spawner/GameLostListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class GameLostListener : GameEventListener
+{
+    public UnityEvent OnLost;
+    public TextMeshProUGUI text;
+
+    public override void OnEventRaised(Object data)
+    {
+        text.text = "You have lost!\nGame will restart in 10 seconds.";
+        OnLost.Invoke();
+    }
+}
