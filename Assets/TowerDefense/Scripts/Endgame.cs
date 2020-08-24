6e41aa9669e2f8c416ee169ca5d4c2c1aa4fc450
SWi <wieczorek0725@gmail.com>
2020-08-24 19:58:09 +0200

    Endgame script, no changes to scene

---
+++ Assets/TowerDefense/Scripts/Endgame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endgame : MonoBehaviour
+{
+    public static Endgame Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ResetScene()
+    {
+        Debug.Log("ResetScene() not implemented");
+    } 
+}
