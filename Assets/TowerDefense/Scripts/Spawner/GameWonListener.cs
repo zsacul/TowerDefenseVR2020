73d14a21432f8d6c49ed31f5241595630b48a808
SWi <wieczorek0725@gmail.com>
2020-09-08 17:25:22 +0200

    Game won event

--- Assets/TowerDefense/Scripts/Spawner/GameWonListener.cs
+++ Assets/TowerDefense/Scripts/Spawner/GameWonListener.cs
@@ -12,7 +12,9 @@ public class GameWonListener : GameEventListener
 
     public override void OnEventRaised(Object data)
     {
-        text.text = "Congratulations! You have won!";
+        text.text = "Congratulations, our castle has been defended!\nGame will restart in 10 seconds.";
+        Endgame.Instance.ResetScene();
+        Debug.Log("Game Won raised in TutorialCanvas.GameLostListener");
         OnWin.Invoke();
     }
 }
